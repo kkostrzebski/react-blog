2d12d47c13e05895d900fa51ea965c681782ab62
Konrad <ko.kostrzebski@gmail.com>
2023-08-22 22:06:39 +0200

    Continue application development

--- src/App.js
+++ src/App.js
@@ -4,13 +4,22 @@ import TopBar from './components/topbar/TopBar'
 import Write from './components/pages/write/Write'
 import Settings from './components/pages/settings/setting'
 import Login from './components/pages/login/Login'
-
+import Register from './components/pages/register/Register'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 function App() {
+	const user = false; 
 	return (
-		<>
+		<Router>
 			<TopBar />
-			<Login />
-		</>
+			<Routes>
+				<Route path='/' element={<Home />} />
+				<Route path="/register" element={user ? <Home/> : <Register />} />
+				<Route path="/login" element={user ? <Home/> : <Login />} />
+				<Route path="/write" element={user ? <Write/> : <Register />} />
+				<Route path="/settings" element={user ? <Settings/> : <Register />} />
+				<Route path="/post/:postId" element={<Single />} />
+			</Routes>
+		</Router>
 	)
 }
 
--- src/components/topbar/TopBar.jsx
+++ src/components/topbar/TopBar.jsx
@@ -1,28 +1,66 @@
 import React from 'react'
 import './topbar.css'
+import { Link } from 'react-router-dom'
 
 export default function TopBar() {
+	const user = true
 	return (
 		<div className='top'>
 			<div className='topLeft'>
 				<i className='topIcon fa-brands fa-square-facebook'></i>
-                <i className="topIcon fa-brands fa-square-twitter"></i>
-                <i className="topIcon fa-brands fa-square-pinterest"></i>
-                <i className="topIcon fa-brands fa-square-instagram"></i>
+				<i className='topIcon fa-brands fa-square-twitter'></i>
+				<i className='topIcon fa-brands fa-square-pinterest'></i>
+				<i className='topIcon fa-brands fa-square-instagram'></i>
 			</div>
 			<div className='topCenter'>
-                <ul className="topList">
-                    <li className='topListItem'>HOME</li>
-                    <li className='topListItem'>ABOUT</li>
-                    <li className='topListItem'>CONTACT</li>
-                    <li className='topListItem'>WRITE</li>
-                    <li className='topListItem'>LOGOUT</li>
-                </ul>
-            </div>
+				<ul className='topList'>
+					<li className='topListItem'>
+						<Link className='link' to='/'>
+							HOME
+						</Link>
+					</li>
+					<li className='topListItem'>
+						<Link className='link' to='/'>
+							ABOUT
+						</Link>
+					</li>
+					<li className='topListItem'>
+						<Link className='link' to='/'>
+							CONTACT
+						</Link>
+					</li>
+					<li className='topListItem'>
+						<Link className='link' to='/write'>
+							WRITE
+						</Link>
+					</li>
+					<li className='topListItem'>{user && 'LOGOUT'}</li>
+				</ul>
+			</div>
 			<div className='topRight'>
-                <img className='topImg' src="https://scontent.fpoz4-1.fna.fbcdn.net/v/t1.15752-9/366089747_255131694082283_1746677504116927587_n.jpg?_nc_cat=102&ccb=1-7&_nc_sid=ae9488&_nc_ohc=VWR3XcqO3p8AX-vO8Bg&_nc_ht=scontent.fpoz4-1.fna&oh=03_AdSG44CUYZjnEKyOly74IKcOKtIqqFs-O3c7PcHN2IN53w&oe=6503ED78" alt="" />
-                <i  className='topSearchIcon fa-solid fa-magnifying-glass'></i>
-            </div>
+				{user ? (
+					<img
+						className='topImg'
+						src='https://scontent.fpoz4-1.fna.fbcdn.net/v/t1.15752-9/366089747_255131694082283_1746677504116927587_n.jpg?_nc_cat=102&ccb=1-7&_nc_sid=ae9488&_nc_ohc=VWR3XcqO3p8AX-vO8Bg&_nc_ht=scontent.fpoz4-1.fna&oh=03_AdSG44CUYZjnEKyOly74IKcOKtIqqFs-O3c7PcHN2IN53w&oe=6503ED78'
+						alt=''
+					/>
+				) : (
+					<ul className='topList'>
+						<li className='topListItem'>
+							<Link className='link' to='/login'>
+								Login
+							</Link>
+						</li>
+						<li className='topListItem'>
+							<Link className='link' to='/register'>
+								Register
+							</Link>
+						</li>
+					</ul>
+				)}
+
+				<i className='topSearchIcon fa-solid fa-magnifying-glass'></i>
+			</div>
 		</div>
 	)
 }
